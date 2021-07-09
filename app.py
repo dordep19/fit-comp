dd67b485ff11835f84e0e72775fc57165d13f3c0
dordep19 <dordep19@gmail.com>
2021-07-09 23:19:04 +0200

    Add and remove users and competitions

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
 
 load_dotenv()
 app = Flask(__name__)
@@ -10,11 +11,75 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import User
+from models import User, Competition, InvalidUsage
 
 @app.route('/')
-def hello():
+def root():
     return 'Welcome to fit-comp'
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.route('/users/add', endpoint='add-user')
+def add_user():
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    gender = request.args.get('gender')
+    try:
+        user = User(
+            first_name = first_name,
+            last_name = last_name,
+            gender = gender
+        )
+        db.session.add(user)
+        db.session.commit()
+        return 'User added (id={})'.format(user.id)
+    except Exception as e:
+	    raise InvalidUsage(str(e), status_code=500)
+
+@app.route('/users/remove/<id_>', endpoint='remove-user')
+def remove_user(id_):
+    try:
+        user = User.query.filter_by(id=id_).one()
+        db.session.delete(user)
+        db.session.commit()
+        return 'User removed (id={})'.format(user.id)
+    except NoResultFound:
+        raise InvalidUsage('User not found', status_code=404)
+    except Exception as e:
+        raise InvalidUsage(str(e), status_code=500)
+
+@app.route('/competitions/add', endpoint='add-comp')
+def add_comp():
+    type = request.args.get('type')
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+    try:
+        comp = Competition(
+            type = type,
+            start_date = start_date,
+            end_date = end_date
+        )
+        db.session.add(comp)
+        db.session.commit()
+        return 'Competition added (id={})'.format(comp.id)
+    except Exception as e:
+	    raise InvalidUsage(str(e), status_code=500)
+
+@app.route('/competitions/remove/<id_>', endpoint='remove-comp')
+def remove_user(id_):
+    try:
+        comp = Competition.query.filter_by(id=id_).one()
+        db.session.delete(comp)
+        db.session.commit()
+        return 'Competition removed (id={})'.format(comp.id)
+    except NoResultFound:
+        raise InvalidUsage('Competition not found', status_code=404)
+    except Exception as e:
+        raise InvalidUsage(str(e), status_code=500)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 from app import db
 
 class User(db.Model):
@@ -103,3 +102,18 @@ class assignments(db.Model):
             'comp_id': self.comp_id,
             'admin': self.admin
         }
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
