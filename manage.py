47b12c3f28572651102d81cfd37c84d1b5a30abe
dordep19 <dordep19@gmail.com>
2021-07-12 14:48:32 +0200

    Dockerize app

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
@@ -7,6 +7,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host='0.0.0.0', port=5000))
 
 if __name__ == '__main__':
     manager.run()
